84b66c4517ac3da4eb0f9b9d594f5d4b97f2fdb2
DiegoVeega <00148816@uca.edu.sv>
2019-03-29 16:54:51 -0600

    funcion shared

--- app/src/main/java/com/diegoveega/evaluacion1/ShareActivity.java
+++ app/src/main/java/com/diegoveega/evaluacion1/ShareActivity.java
@@ -1,15 +1,28 @@
 package com.diegoveega.evaluacion1;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class ShareActivity extends AppCompatActivity {
 
-
+    private String Aux_Text;
+    TextView mShared;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_share);
+
+        mShared=findViewById(R.id.shared);
+
+        Intent Intent_shared=getIntent();
+
+        if(Intent_shared != null){
+            Aux_Text = Intent_shared.getStringExtra(Intent.EXTRA_TEXT);
+        }
+
+        mShared.setText(Aux_Text);
     }
 }
